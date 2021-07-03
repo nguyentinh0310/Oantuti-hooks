b843d8fea02c56689b44a5eca514ee335f9b0dda
Your Name <nguyenquoctinh0310@gmail.com>
2021-07-03 16:52:23 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import largeHand from "./images/hand-96.png";
 import largeScissors from "./images/scissor-96.png";
 import largeRock from "./images/rock-96.png";
@@ -13,6 +13,7 @@ App.propTypes = {
 App.defaultProps = {
   images: [largeHand, largeScissors, largeRock], // lá, kéo,búa
 };
+
 function App(props) {
   const { images } = props;
   let [playerScore, setPlayerScore] = useState(0);
@@ -28,51 +29,55 @@ function App(props) {
   const randomGenerate = () => {
     const randomPick = Math.floor(Math.random() * images.length);
     setRandomPick(randomPick);
-    checkWinner();
+    console.log("randomPick", randomPick);
   };
 
-  const checkWinner = () => {
-    // let { playerScore, emptyScore, playerPick, randomPick } = props;
-    if (playerPick === randomPick) {
-      // Hòa
-    } else {
-      // người dùng chọn lá
-      if (playerPick === 0) {
-        // máy chọn búa => máy thua
-        if (randomPick === 2) {
-          playerScore++;
+  useEffect(() => {
+    const checkWinner = () => {
+      // let { playerScore, emptyScore, playerPick, randomPick } = props;
+      if (playerPick === randomPick) {
+        // Hòa
+        return;
+      } else {
+        // người dùng chọn lá
+        if (playerPick === 0) {
+          // máy chọn búa => máy thua
+          if (randomPick === 2) {
+            playerScore++;
+            // máy chọn kéo => máy thắng
+          } else if (randomPick === 1) {
+            emptyScore++;
+          }
         }
-        // máy chọn kéo => máy thắng
-        else if (randomPick === 1) {
-          emptyScore++;
+        // người dùng chọn kéo
+        else if (playerPick === 1) {
+          // máy chọn lá => máy thua
+          if (randomPick === 0) {
+            playerScore++;
+          }
+          // máy chọn búa => máy thắng
+          else if (randomPick === 2) {
+            emptyScore++;
+          }
+        }
+        // người dùng chọn búa
+        else if (playerPick === 2) {
+          // máy chọn lá => máy thắng
+          if (randomPick === 0) {
+            emptyScore++;
+          }
+          // máy chọn kéo => máy thua
+          else if (randomPick === 1) {
+            playerScore++;
+          }
         }
       }
-      // người dùng chọn kéo
-      else if (playerPick === 1) {
-        // máy chọn lá => máy thua
-        if (randomPick === 0) {
-          playerScore++;
-        }
-        // máy chọn búa => máy thắng
-        else if (randomPick === 2) {
-          emptyScore++;
-        }
-      }
-      // người dùng chọn búa
-      else if (playerPick === 2) {
-        // máy chọn lá => máy thua
-        if (randomPick === 0) {
-          playerScore++;
-        }
-        // máy chọn kéo => máy thắng
-        else if (randomPick === 1) {
-          emptyScore++;
-        }
-      }
-    }
-    setPlayerScore(playerScore);
-    setEmptyScore(emptyScore);
-  };
+      setPlayerScore(playerScore);
+      setEmptyScore(emptyScore);
+    };
+    checkWinner();
+    // eslint-disable-next-line
+  }, [playerPick, randomPick]);
 
   return (
     <Fragment>
@@ -80,9 +85,9 @@ function App(props) {
         <Computer image={images[randomPick]} />
       </section>
       <section className="section">
-        <h1 className="title has-text-primary has-text-centered">
-          Player: {playerScore} -{" "}
-          <span className="has-text-danger">Computer: {emptyScore}</span>
+        <h1>
+          <span className="player">Player: {playerScore}</span> -&nbsp;
+          <span className="computer">Computer: {emptyScore}</span>
         </h1>
       </section>
       <section className="section">
